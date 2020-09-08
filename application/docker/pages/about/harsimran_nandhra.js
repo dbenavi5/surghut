44631afaefebc798b2cba3010404beb41462b5d1
Harsimran Nandhra <simrankaur1435@gmail.com>
2020-09-08 14:36:57 -0700

    harsimran.js

--- application/docker/pages/about/harsimran_nandhra.js
+++ application/docker/pages/about/harsimran_nandhra.js
@@ -5,12 +5,14 @@ function harsimran_nandhra() {
   return (
     <>
       <div>
-        <h1>Harsimran NANDHRA</h1>
+        <h1>Harsimran Nandhra</h1>
       </div>
 
       <div>
-        <h2>Role</h2>
-        <p>I will work on the front-end</p>
+        <h2>Role:</h2>
+        <p>I will work on the front-end and help with the back-end.</p>
+        <h2>Link </h2>
+        <a href="https://github.com/hnandhra1"> my Github</a>
         <div>
           <Link href="/about">
             <a>back to about page</a>
