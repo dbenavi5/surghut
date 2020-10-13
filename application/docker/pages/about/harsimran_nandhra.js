0c634ed9826c7afbd19d19944c7b889895a92b60
Pierre antoine <pierre.antoine@epitech.eu>
2020-10-13 09:00:53 +0200

    correction build: remove Harsimran css file

--- application/docker/pages/about/harsimran_nandhra.js
+++ application/docker/pages/about/harsimran_nandhra.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 
-import styles from './harsimran.css'
+//import styles from './harsimran.css'
 function harsimran_nandhra() {
   return (
     <>
