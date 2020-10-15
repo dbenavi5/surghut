bca058269859e56513913cf15bd5beff475cd94c
Pierre antoine <pierre.antoine@epitech.eu>
2020-10-15 22:00:39 +0200

    add geolocalisation

--- application/docker/component/Map.js
+++ application/docker/component/Map.js
@@ -1,34 +1,51 @@
 import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
+import { geolocated } from "react-geolocated";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const Mark = ({ src }) => <img src={src}/>;
 
 function Map(props) {
 
-    const [center, setCenter] = useState(
-        {
-            lat: 59.95,
-            lng: 30.33
-        }
-    );
     const [zoom, setZoom] = useState(11);
 
+    
+    if (props.coords) {
+        const center = {
+            lat: props.coords.latitude,
+            lng: props.coords.longitude
+        }
+        return (
+            // Important! Always set the container height explicitly
+            <div style={{ height: '100vh', width: '100%' }}>
+                <GoogleMapReact
+                bootstrapURLKeys={{ key: "AIzaSyCKcBbyY43_ocqIJQUmQlSHZopPH-TPhuA" }}
+                defaultCenter={center}
+                defaultZoom={zoom}
+                >
+                    <Mark
+                      lat={props.coords.latitude}
+                      lng={props.coords.longitude}
+                      src="https://image.flaticon.com/icons/png/128/3603/3603850.png"
+                    />
+                </GoogleMapReact>
+            </div>
+        )
+    }
+
+    if (!props.isGeolocationAvailable) {
+        return (
+            <div>Your browser does not support Geolocation</div>
+        )
+    }
+
     return (
-        // Important! Always set the container height explicitly
-        <div style={{ height: '100vh', width: '100%' }}>
-            <GoogleMapReact
-            bootstrapURLKeys={{ key: "AIzaSyCKcBbyY43_ocqIJQUmQlSHZopPH-TPhuA" }}
-            defaultCenter={center}
-            defaultZoom={zoom}
-            >
-                <AnyReactComponent
-                  lat={59.955413}
-                  lng={30.337844}
-                  text="My Marker"
-                />
-            </GoogleMapReact>
-        </div>
+        <div>Geolocation is not enabled</div>
     )
 }
 
-export default Map;
+export default geolocated({
+    positionOptions: {
+        enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 5000,
+})(Map);;
