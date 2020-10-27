6a5bbb5a6f390133b9d0c07e340fd4b884b415d9
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2020-10-26 22:25:00 -0700

    changing css in navbar

--- application/docker/components/Navbar.js
+++ application/docker/components/Navbar.js
@@ -4,11 +4,11 @@ import Link from 'next/link';
 function Navbar () {
 
     return (
-        <nav className="navbar navbar-expand navbar-dark bg-dark mb-4">
+        <nav className="navbar">
             <div className="container">
-                <Link href="/"><a className="navbar-brand">SurgeHut</a></Link>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav ml-auto">
+                <Link href="/"><a className="navbar_brand">SurgeHut</a></Link>
+                <div className="collapse-navbar">
+                    <ul className="navbar-nav">
                         <li className="nav-item">
                             <Link href="/about"><a className="nav-link">About</a></Link>
                         </li>
@@ -20,11 +20,11 @@ function Navbar () {
                         </li>
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Category</a>
-                            <div className="drop-content">
+                            {/* <div className="drop-content">
                                 <a className="dropdown-item" href="#">County</a>
                                 <a className="dropdown-item" href="/covid_page">COVID-19</a>
                                 <a className="dropdown-item" href="/wildfires_page">Wildfires</a>
-                            </div>
+                            </div> */}
                         </li>   
                     </ul>
                 </div>
--- application/docker/pages/login.js
+++ application/docker/pages/login.js
@@ -9,27 +9,29 @@ const Login = () => (
             <title>SurgeHut</title>
             <meta name="Description" content="SFSU team3  project Fall 2020."></meta>
             <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-            <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css"/>
+            {/* <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css"/> */}
             <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
             <script src="https://apis.google.com/js/platform.js" async defer></script>
         </Head>
+
+        
         <div>
             <center>
-                <h1>Login Page</h1>
-                <form id= "form1" name="form1" >
+                <form className="reg-form" id= "form1" name="form1" >
+                    <h1 className= "reg_border">Login Page</h1>
                     <fieldset className="field">
                         <div>
                             <div><label htmlFor="username">Username</label></div>
-                            <input id = "username" name="username" type="text" placeholder="Username"
+                            <input className="reg-inputs" id = "username" name="username" type="text" placeholder="Username"
                                 size="15" required/>
                         </div>
                         
                         <div>
                             <div><label htmlFor="password">Password</label></div>
-                            <input name="password" type="password" placeholder="Password"
+                            <input className="reg-inputs" name="password" type="password" placeholder="Password"
                                 size="15"required/>
                         </div>
-                        <div>
+                        <div className="sumbit-wrap">
                             <input id="submit" className="submit" type="submit" value="Login" />
                             <input id="reset" className="reset" type="reset" value="Cancel"/>
                         </div>
