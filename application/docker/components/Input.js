d918f847c4495dc3fa702ad30a69ef4dc94ad85b
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2020-11-11 22:31:55 -0800

    editting input boxes

--- application/docker/components/Input.js
+++ application/docker/components/Input.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Search.module.css';
+import styles from './Form.module.css';
 
 function SearchTextInput({value, onTextChange, placeHolder, type}) {
   const eventHandler = (event) => {
