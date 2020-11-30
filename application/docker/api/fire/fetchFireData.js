1ed3e46ebac109c6a806486b8ca5abddff40c20e
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2020-11-30 03:01:42 -0800

    data

--- application/docker/api/fire/fetchFireData.js
+++ application/docker/api/fire/fetchFireData.js
@@ -1,13 +1,70 @@
 import axios from 'axios';
 
-const url = "https://raw.githubusercontent.com/CSC-648-SFSU/csc648-02-fa20-team03/master/application/docker/csv_files/fire_data.csv?token=ANLI5K7IU5ANAAMGADO5W6S7ZUKAQ"
+const url = "https://raw.githubusercontent.com/CSC-648-SFSU/csc648-02-fa20-team03/master/application/docker/json_files/fire_data.json?token=ANLI5K42WJ3IVOJYNL563S27ZYFBG";
+//"https://raw.githubusercontent.com/CSC-648-SFSU/csc648-02-fa20-team03/master/application/docker/json_files/fire_data.json?token=ANLI5K4LDYYXF75KDHBSYRS7ZUJ4G";
+// "https://raw.githubusercontent.com/CSC-648-SFSU/csc648-02-fa20-team03/master/application/docker/csv_files/fire_data.csv?token=ANLI5K4ZUEURZB2Q6IWNJUC7ZUU2E";
+
 export const fetchFireData = async () => {
     try {
-        const res = await fetch(url);
-        const data = await res.json();
-
-        const {incident_name, incident_county, incident_location, incident_acres_burned, incident_containment} = data;
+        const  { data } = await axios.get(url);
+        return data;
     } catch (error) {
         
     }
 }
+// import React, { useState } from 'react';
+// import ReactDOM from 'react-dom';
+// import axios from 'axios';
+// // import ScotchInfoBar from './ScotchInfoBar';
+// import './styles.css';
+
+// function App() {
+//   const [names, setNames] = useState(null);
+
+//   const fetchData = async () => {
+//     const response = await axios.get(
+//       'https://raw.githubusercontent.com/CSC-648-SFSU/csc648-02-fa20-team03/master/application/docker/json_files/fire_data.json?token=ANLI5K4LDYYXF75KDHBSYRS7ZUJ4G'
+//     );
+
+//     setNames(response.data);
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>wildfires</h1>
+//       <h2>Fetch a list from an API and display it</h2>
+
+//       {/* Fetch data from API */}
+//       <div>
+//         <button className="fetch-button" onClick={fetchData}>
+//           Fetch Data
+//         </button>
+//         <br />
+//       </div>
+
+//       {/* Display data from API */}
+//       <div className="books">
+//         {names &&
+//           names.map((name, index) => {
+//             const fireName = name.incident_name.join(", ");
+//             const county = name.incident_county.join(", ");
+//             const otherCounties = name.incident_location.join(", ");
+
+//             return (
+//               <div className="book" key={index}>
+//                 <h2>fire name:{fireName}</h2>
+
+//                 <div className="details">
+//                     <p>county:{county}</p>
+//                     <p>locations:{otherCounties}</p>
+//                 </div>
+//               </div>
+//             );
+//           })}
+//       </div>
+//     </div>
+//   );
+// }
+
+// const rootElement = document.getElementById('root');
+// ReactDOM.render(<App />, rootElement);
--- application/docker/components/FireGrid.js
+++ application/docker/components/FireGrid.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Loading from '../components/Loading';
+// import Loading from '../components/Loading';
 import styles from './FireGrid.module.css';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
-const FireGrid = (props) => {
+const FireGrid = ({incident_name, incident_county, incident_location}) => {
     //console.log(props);
 
     return (
@@ -11,11 +11,11 @@ const FireGrid = (props) => {
             <Grid container spacing={3} justify="center">
                 <Grid item component={Card}>
                     <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Fire Name: </Typography>
-                        <Typography variant="h5" gutterBottom> County: </Typography>
-                        <Typography color="textSecondary" gutterBottom> Counties Affected:</Typography>
-                        <Typography color="textSecondary" gutterBottom> Acres Burned: </Typography>
-                        <Typography variant="body2" gutterBottom> Fire Containment: </Typography>
+                        <Typography color="textSecondary" gutterBottom>Fire Name: {incident_name}</Typography>
+                        <Typography variant="h5" gutterBottom>County: {incident_county}</Typography>
+                        <Typography color="textSecondary" gutterBottom>Counties Affected: {incident_location}</Typography>
+                        {/* <Typography color="textSecondary" gutterBottom> Acres Burned: </Typography>
+                        <Typography variant="body2" gutterBottom> Fire Containment: </Typography> */}
                     </CardContent>
                 </Grid>
             </Grid>
