94e8a56e36aa94bb517d5b4c390210cbd624dc31
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2020-11-30 06:24:10 -0800

    push to new-db branch

--- application/docker/api/fire/fetchFireData.js
+++ application/docker/api/fire/fetchFireData.js
@@ -1,70 +1,15 @@
 import axios from 'axios';
 
-const url = "https://raw.githubusercontent.com/CSC-648-SFSU/csc648-02-fa20-team03/master/application/docker/json_files/fire_data.json?token=ANLI5K42WJ3IVOJYNL563S27ZYFBG";
-//"https://raw.githubusercontent.com/CSC-648-SFSU/csc648-02-fa20-team03/master/application/docker/json_files/fire_data.json?token=ANLI5K4LDYYXF75KDHBSYRS7ZUJ4G";
-// "https://raw.githubusercontent.com/CSC-648-SFSU/csc648-02-fa20-team03/master/application/docker/csv_files/fire_data.csv?token=ANLI5K4ZUEURZB2Q6IWNJUC7ZUU2E";
+const url = "https://www.fire.ca.gov/umbraco/api/IncidentApi/List?inactive=true";
+//"https://raw.githubusercontent.com/CSC-648-SFSU/csc648-02-fa20-team03/master/application/docker/json_files/fire_data.json?token=ANLI5K42WJ3IVOJYNL563S27ZYFBG";
 
 export const fetchFireData = async () => {
     try {
-        const  { data } = await axios.get(url);
-        return data;
+        const data  = await axios.get(url);
+
+        console.log(data);
+        //return {data};
     } catch (error) {
         
     }
 }
-// import React, { useState } from 'react';
-// import ReactDOM from 'react-dom';
-// import axios from 'axios';
-// // import ScotchInfoBar from './ScotchInfoBar';
-// import './styles.css';
-
-// function App() {
-//   const [names, setNames] = useState(null);
-
-//   const fetchData = async () => {
-//     const response = await axios.get(
-//       'https://raw.githubusercontent.com/CSC-648-SFSU/csc648-02-fa20-team03/master/application/docker/json_files/fire_data.json?token=ANLI5K4LDYYXF75KDHBSYRS7ZUJ4G'
-//     );
-
-//     setNames(response.data);
-//   };
-
-//   return (
-//     <div className="App">
-//       <h1>wildfires</h1>
-//       <h2>Fetch a list from an API and display it</h2>
-
-//       {/* Fetch data from API */}
-//       <div>
-//         <button className="fetch-button" onClick={fetchData}>
-//           Fetch Data
-//         </button>
-//         <br />
-//       </div>
-
-//       {/* Display data from API */}
-//       <div className="books">
-//         {names &&
-//           names.map((name, index) => {
-//             const fireName = name.incident_name.join(", ");
-//             const county = name.incident_county.join(", ");
-//             const otherCounties = name.incident_location.join(", ");
-
-//             return (
-//               <div className="book" key={index}>
-//                 <h2>fire name:{fireName}</h2>
-
-//                 <div className="details">
-//                     <p>county:{county}</p>
-//                     <p>locations:{otherCounties}</p>
-//                 </div>
-//               </div>
-//             );
-//           })}
-//       </div>
-//     </div>
-//   );
-// }
-
-// const rootElement = document.getElementById('root');
-// ReactDOM.render(<App />, rootElement);
--- application/docker/components/Wildfire.js
+++ application/docker/components/Wildfire.js
@@ -1,87 +1,62 @@
-import React, { Component } from 'react';
-// import FireGrid from './FireGrid';
-import styles from './FireGrid.module.css';
-import { fetchFireData } from '../api/fire/fetchFireData';
-// import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import React, {Component} from 'react';
+// import FireGrid from '../components/FireGrid';
+// import { fetchFireData } from '../api/fire/fetchFireData';
 
-class Wildfire extends Component {
-    
-    state = {
-        data: {},
+class Wildfire extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            data: [],
+            name: [],
+        }
     }
+
+    // url = "https://www.fire.ca.gov/umbraco/api/IncidentApi/List?inactive=true";
+
     async componentDidMount() {
-        const fetchedData = await fetchFireData();
-        console.log(fetchedData);
+        fetch("https://www.fire.ca.gov/umbraco/api/IncidentApi/List?inactive=2020")
+            .then(response => 
+                response.json(console.log(response))) 
+            .then(
+                (Name) => {
+                    this.setState({
+                        isLoading: true,
+                        data: Name.data
+                    });
+                },
+
+                (error) => {
+                    this.setState({
+                        isLoading: true,
+                        error
+                    });
+                }
+            )
     }
+    
     render(){
-        const { data } = this.state;
-        return(
-            <div className={styles.container}>
-                <p>fire name:{data.incident_name}</p>
-                <p>county:{data.incident_county}</p>
-                <p>locations:{data.incident_location}</p>
-                <p>acres burned:{data.incident_acres_burned}</p>
-                <p>containment:{data.incident_containment}</p>
-            </div>
-        )
+
+        const {error, isLoaded, data }= this.state;
+
+        if(error) {
+            return <div>Error: {error.message}</div>
+        } else if (!isLoaded){
+            return <div>Loading</div>
+        } else {
+            return(
+                <ul>
+                    {data.map(item => (
+                        <li key={item.Name}>
+                            {item.Name}
+                        </li>
+                    ))}
+                </ul>
+            )
+        }
+
     }
 }
+
 export default Wildfire;
-
-// state = {
-//     incident_name: [],
-//     incident_county:[],
-//     incident_location: [],
-// };
-
-// url = "https://raw.githubusercontent.com/CSC-648-SFSU/csc648-02-fa20-team03/master/application/docker/csv_files/fire_data.csv?token=ANLI5K4ZUEURZB2Q6IWNJUC7ZUU2E";
-
-// async componentDidMount() {
-
-//     const res = await axios.get(this.url);  
-//     const rows = res.data.split("\n");
-    
-//     const fire_name = [];
-//     const incident_county = [];
-//     const incident_location = [];
-
-//     for (let i = 1; i < rows.length; i++) {
-//         const row = rows[i].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-//         const fireName = row[0].replace(/"/g,"");
-//         const countyName = row[1].replace(/"/g,"");
-//         const fireLocation = row[2].replace(/"/g,"");
-
-//         if(fireName !== "") {
-//             fire_name.push({
-//                 incident_name: fireName,
-//                 incident_county: countyName,
-//                 incident_location: fireLocation,
-//             });
-//         }
-//     }
-//     //console.table(fire_name);
-
-//     this.setState({fire_name, incident_county, incident_location });
-
-// }
-// render(){
-
-//     const { incident_name, incident_county, incident_location } = this.state;
-
-//     return(
-//         <div className={styles.container}>
-//             {incident_name.map((name) => {
-//                 return(
-//                     <div>
-//                         <p>fire name:{name.incident_name}</p>
-//                         <p>county:{name.incident_county}</p>
-//                         <p>locations:{name.incident_location}</p>
-//                     </div>
-//                 )
-//             })}
-                    
-//         </div>
-//     )
-// }
-
-// }
